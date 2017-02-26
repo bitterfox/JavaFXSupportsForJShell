d1d739b9b291a4f66914aa83472f65b93a24d5b1
bitter_fox <bitterfoxc@gmail.com>
2017-02-26 15:53:31 +0900

    Add startup

--- scripts/jfxshell.sh
+++ scripts/jfxshell.sh
@@ -1,7 +1,9 @@
 #JAVA_HOME=
 
-J_MOD_PATH="./JavaFXSupportsForJShell.jar"
-R_MOD_PATH="./JavaFXSupportsForJShell.jar"
+PROJ_ROOT=.
 
-$JAVA_HOME/bin/jshell -J--module-path -J$J_MOD_PATH -R--module-path -R$R_MOD_PATH --execution "javafx" $@
+J_MOD_PATH=$PROJ_ROOT/target/JavaFXSupportsForJShell.jar
+R_MOD_PATH=$PROJ_ROOT/target/JavaFXSupportsForJShell.jar
+STARTUP=$PROJ_ROOT/scripts/startup.jsh
 
+$JAVA_HOME/bin/jshell -J--module-path -J$J_MOD_PATH -R--module-path -R$R_MOD_PATH --execution "javafx" --startup $STARTUP $@
