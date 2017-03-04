0d143e78e3e710937bab8eeab77f7685f833a8cb
bitter_fox <bitterfoxc@gmail.com>
2017-03-04 09:54:37 +0900

    Fix compile script to remove target dir first

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,5 +4,5 @@
 
 PROJ_ROOT=.
 
-cd $PROJ_ROOT; $JAVA_HOME/bin/javac --source-path "src/main/java" -d "target/classes" src/main/java/module-info.java
+cd $PROJ_ROOT; rm -rf target ; $JAVA_HOME/bin/javac --source-path "src/main/java" -d "target/classes" src/main/java/module-info.java
 
